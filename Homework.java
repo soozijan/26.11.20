8c8919dacf170efa069e484d6495b717486cf5ef
soozijan <75090137+soozijan@users.noreply.github.com>
2020-11-26 19:46:02 +0200

    Create Homework.java

---
+++ Homework.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import javax.lang.model.element.Name;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+   /*     System.out.println("helloworld");
+//java.util.Scanner s=new Scanner(System.in);//chinese
+        String name = s.nextLine(); //japanese
+
+        System.out.print("HELLO");
+        System.out.print(name);
+        System.out.println("How are you today?");
+
+        // Print to the user question : "What day is today?"
+        // Input day
+        // i.e. Thursday
+        //Print Today is Thursday
+
+        System.out.print("What day is today?");
+
+    */
+
+int x;
+x = 785 * 231;
+System.out .println(x);
+
+double cook = (4.3 * 7.8);
+        System.out.println(cook);
+
+
+
+        char lettx = 'x';
+        System.out.println(lettx);
+
+        char letty = 'y';
+        System.out.println(letty);
+
+        short xs=1, ys=2, zs=3;
+        System.out.println(short);
+        xs1 = ys+zs;
+        System.out.println(short)
+
+
+    }
+}
