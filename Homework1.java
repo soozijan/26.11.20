d2a170fb295226665f9d173ff150a7d94a59a67d
soozijan <75090137+soozijan@users.noreply.github.com>
2020-11-30 16:21:52 +0200

    Create Homework1.java

---
+++ Homework1.java
@@ -0,0 +1,14 @@
+     //Targil 1:
+        String id = "317436884";
+
+        //Targil 2:
+         String full_name = "sanaz" + " " + "toobian";
+
+         //Targil 3:
+        String adress = "deganya" + " " + "12" + " " + "petah tikva";
+
+        //Targil 4:
+        String details = id + " " + full_name + " " + adress;
+
+        //Targil 5:
+        System.out.println(details);
